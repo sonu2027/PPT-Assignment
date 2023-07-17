dccb56ab33e448463e8765154d8242bbaa4049d4
Sonu Mondal <111413339+sonu2027@users.noreply.github.com>
2023-07-17 12:47:51 +0530

    Update script.js

--- Web dev assignment/Assignment-07/02_Todos web app/script.js
+++ Web dev assignment/Assignment-07/02_Todos web app/script.js
@@ -36,14 +36,13 @@ let update=document.getElementById("update");
 inputbutton[3].addEventListener("click", edittask);
 function edittask(){
     inputbutton[2].innerText="Mark Completed"
-    para[0].innerText="Task";
+    // para[0].innerText=input[0].value;
     para[1].innerText="Status: Pending";
     inputbutton[2].style.background="rgb(50, 238, 50)";
     input[0].placeholder="Edit your task";
-    input[0].value="";
+    // input[0].value="";
     update.style.display="block";
 }
-
 inputbutton[4].addEventListener("click", updateinput);
 function updateinput(){
     para[0].innerText=input[1].value;
@@ -52,6 +51,8 @@ function updateinput(){
 }
 inputbutton[5].addEventListener("click", close);
 function close(){
+    input[1].value="";
     update.style.display="none";
+
 }
 
